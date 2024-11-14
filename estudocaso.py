31ef8185fb93bbf71be3e84e5a05646ea1118046
JulianaFachiniGomes <jubfachini@gmail.com>
2024-11-14 10:20:53 -0300

    Update estudocaso.py

--- estudocaso.py
+++ estudocaso.py
@@ -2,3 +2,20 @@ import streamlit as st
 import pandas as pd
 
 st.title('Localização das comunidades quilombolas (2002)')
+
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+#retirar o unnamed
+df.drop(columns=['Unnamed: 0'],inplace= True) 
+
+
+#converter lat e long para numeros
+list = ['Lat_d', 'Long_d']
+df[list] = df[list].apply(pd.to_numeric, errors='coerce') 
+
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+
+
+
